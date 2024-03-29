37a57eb96d4c3e8e60aa60e21eb49a8d8cf2c376
sawan1k <sawankushwaha222@gmail.com>
2024-03-29 19:21:32 +0530

    StyleSheet api

--- App.js
+++ App.js
@@ -1,11 +1,21 @@
-import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Jai Shree Ram!</Text>
-      <StatusBar style="auto" />
+      <View style={[styles.box, styles.lightblueBg, styles.boxShadow]}>
+        <Text style={{ borderRadius: 5, backgroundColor: "red" }}>
+          Lightblue box
+        </Text>
+      </View>
+      <View style={[styles.box, styles.lightgreenBg, styles.androidShadow]}>
+        <Text>Lightgreen box</Text>
+      </View>
+      <View style={styles.darkMode}>
+        <Text style={styles.darkModeText}>
+          Style inheritance <Text style={styles.boldText}>in bold</Text>
+        </Text>
+      </View>
     </View>
   );
 }
@@ -13,8 +23,41 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: "plum",
+    padding: 60,
+  },
+  box: {
+    width: 250,
+    height: 250,
+    // padding: 10,
+    paddingHorizontal: 10,
+    paddingVertical: 20,
+    marginVertical: 10,
+    borderWidth: 2,
+    borderColor: "purple",
+  },
+  lightblueBg: {
+    backgroundColor: "lightblue",
+  },
+  lightgreenBg: {
+    backgroundColor: "lightgreen",
+  },
+  boxShadow: {
+    shadowColor: "#333333",
+    shadowOffset: { width: 6, height: 6 },
+    shadowOpacity: 0.6,
+    shadowRadius: 4,
+  },
+  androidShadow: {
+    elevation: 10,
+  },
+  darkMode: {
+    backgroundColor: "black",
+  },
+  darkModeText: {
+    color: "white",
+  },
+  boldText: {
+    fontWeight: "bold",
   },
 });
