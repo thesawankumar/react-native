e0e3ab02c2a4a9fe53a2ae90b40571619236cc82
sawan1k <sawankushwaha222@gmail.com>
2024-03-29 16:22:49 +0530

    Text completed

--- CoreComponents/App.js
+++ CoreComponents/App.js
@@ -1,13 +1,10 @@
-import { View } from "react-native";
+import { View, Text } from "react-native";
 export default function App() {
   return (
-    <View style={{ flex: 1, backgroundColor: "plum" }}>
-      <View
-        style={{ width: 200, height: 200, backgroundColor: "lightblue" }}
-      ></View>
-      <View
-        style={{ width: 200, height: 200, backgroundColor: "lightgreen" }}
-      ></View>
+    <View style={{ flex: 1, backgroundColor: "plum", padding: 60 }}>
+      <Text>
+        <Text style={{ color: "white" }}>jai </Text>shree ram
+      </Text>
     </View>
   );
 }
