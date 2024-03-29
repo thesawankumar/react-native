fcd0c0bd10afd4e9dac131eead0cda121ebb0172
sawan1k <sawankushwaha222@gmail.com>
2024-03-29 16:57:44 +0530

    Image Completed

--- CoreComponents/App.js
+++ CoreComponents/App.js
@@ -1,10 +1,17 @@
-import { View, Text } from "react-native";
+import { View, Text, Image, ImageBackground } from "react-native";
+const logo = require("./assets/adaptive-icon.png");
 export default function App() {
   return (
     <View style={{ flex: 1, backgroundColor: "plum", padding: 60 }}>
-      <Text>
-        <Text style={{ color: "white" }}>jai </Text>shree ram
-      </Text>
+      {/* <Image source={logo} style={{ width: 300, height: 300 }} />
+      <Image
+        source={{ uri: "https://picsum.photos/300" }}
+        style={{ width: 300, height: 300 }}
+      /> */}
+
+      <ImageBackground source={logo} style={{ flex: 1 }}>
+        <Text>Image text </Text>
+      </ImageBackground>
     </View>
   );
 }
