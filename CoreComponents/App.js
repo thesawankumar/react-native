d7f0f756cd962edf4ef67c03e2d21bab6ba04953
sawan1k <sawankushwaha222@gmail.com>
2024-03-29 16:17:58 +0530

    View completed

---
+++ CoreComponents/App.js
@@ -0,0 +1,13 @@
+import { View } from "react-native";
+export default function App() {
+  return (
+    <View style={{ flex: 1, backgroundColor: "plum" }}>
+      <View
+        style={{ width: 200, height: 200, backgroundColor: "lightblue" }}
+      ></View>
+      <View
+        style={{ width: 200, height: 200, backgroundColor: "lightgreen" }}
+      ></View>
+    </View>
+  );
+}
